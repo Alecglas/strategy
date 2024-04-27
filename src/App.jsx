688c5d566fda50c514c3113b209dd09bce6dd5b8
Alecglas <alecglas@gmail.com>
2024-04-27 00:01:22 -0500

    adding saving

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import io from 'socket.io-client'
 
-const url = import.meta.env.DEV ? 'https://alecglascock.com' : 'https://alecglascock.com'
+const url = import.meta.env.DEV ? 'localhost:8443' : 'https://alecglascock.com'
 
 const socket = io(url)
 
--- src/server.cjs
+++ src/server.cjs
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,6 +12,15 @@ const io = new socketIo.Server(server, {
 });
 
 let counter = 0
+fs.access('./saves/counter.json', fs.constants.R_OK, (err) => {
+    if (err) {
+        fs.appendFile('./saves/counter.json', JSON.stringify({ counter: 0 }, null, 2), (err) => {
+
+        });
+    } else {
+        counter = JSON.parse(fs.readFileSync('./saves/counter.json', 'utf-8')).counter
+    }
+})
 
 io.on('connection', (socket) => {
     console.log("connection established")
@@ -20,6 +30,13 @@ io.on('connection', (socket) => {
         io.emit('gameState', { counter })
     });
 });
+
+const handleServerShutdown = () => {
+    fs.writeFileSync('./saves/counter.json', JSON.stringify({ counter }, null, 2));
+}
+
+process.on('SIGINT', handleServerShutdown);
+process.on('SIGTERM', handleServerShutdown);
 
 server.listen(8443, () => {
     console.log("Socket.io server running")
