51aa00176a4abde29def1dfd941c3f2b2cf59f1b
Alecglas <alecglas@gmail.com>
2025-09-28 01:46:29 -0500

    styling changes

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -17,8 +17,6 @@ function Chat() {
                 messages.shift()
             }
         })
-
-
     }, [])
 
     const handleKeyDown = (e) => {
@@ -30,16 +28,31 @@ function Chat() {
     }
 
     return connected && user && (
-        <div className="chatBox">
-            <label className="messageBox">
-                <textarea onKeyDown={handleKeyDown} className="messageBox" type="text" name="username"
-                          value={currentMessage}
-                          onChange={e => setCurrentMessage(e.target.value)}/>
+        <div className="fixed bottom-0 right-0 w-80 h-full bg-gray-900 flex flex-col rounded-tl-md">
+            <div className="flex-1 overflow-y-auto p-2 flex flex-col-reverse text-white ">
+                {messages
+                    .slice()
+                    .reverse()
+                    .map((message, i) => (
+                    <div key={i} className="text-left mb-1">
+                        {message}
+                    </div>
+                ))}
+            </div>
+
+
+            <label className="w-full">
+                <textarea
+                    onKeyDown={handleKeyDown}
+                    name="username"
+                    value={currentMessage}
+                    onChange={(e) => setCurrentMessage(e.target.value)}
+                    rows={2}
+                    className="w-full p-2 bg-gray-800 text-white resize-none border-t border-gray-700 focus:outline-none"
+                />
             </label>
-            {messages.toReversed().map((message, i) => (
-                <div className="chatMessage" key={i}>{message}</div>
-            ))}
         </div>
+
     )
 }
 
